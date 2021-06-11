9bd42365bac4cac60ca4dccc01e10a6bcd0876a0
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2021-06-11 13:44:35 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,26 @@
+// From a set of permutations for a given number n find the number of elements such hat no element appears in its original position.
+package challenge;
+
+import java.util.*;
+
+public class Q1 {
+    static int countPer(int n) {
+
+        if (n == 1)
+            return 0;
+        if (n == 2)
+            return 1;
+
+        return (n - 1) * (countPer(n - 1) + countPer(n - 2));
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        System.out.println(countPer(n));
+
+        sc.close();
+    }
+}
